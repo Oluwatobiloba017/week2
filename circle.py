bd5061dba977492c624546dbe3e5dce958374032
Oluwatobiloba017 <26255734@students.lincoln.ac.uk>
2023-02-06 15:04:11 +0000

    added files

--- circle.py
+++ circle.py
@@ -7,4 +7,18 @@ r = float(input())
 
 ob = circle()
 c = ob.findCircum(r)
+
 print("\nCircumference = {:.2f}".format(c))
+
+class pie:
+    def area(self, rad):
+        return  3.14 * rad**2
+
+print("Enter Radius of Circle: ", end="")
+r = float(input())
+
+ob = pie()
+c = ob.area(r)
+
+print("\nCircumference = {:.2f}".format(c))
+
